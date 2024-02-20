a25e0404cab6814ece925e1e59f82d3b2ac7aa0a
Lukas Ehlers <lukas.ehlers@vougee.gmbh>
2024-02-20 08:59:29 +0100

    correcting wrong import links

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,12 +6,12 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.workflow.bad-practices" },
+  { import = "astrocommunity.workflow.bad-practices-nvim" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.worflow.hardtime-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
 }
