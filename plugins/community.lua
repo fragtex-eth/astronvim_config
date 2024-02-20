68df499cc522e91329980feaa41bcf2ba35c4f9c
Lukas Ehlers <lukas.ehlers@vougee.gmbh>
2024-02-20 08:53:20 +0100

    adding community packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,11 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.workflow.bad-practices" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.docker" },
 }
