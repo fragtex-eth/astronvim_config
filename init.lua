726adf91c4f6a1613b9f8a912daab47c0c524d5f
LukasVG7 <153903978+LukasVG7@users.noreply.github.com>
2024-02-26 12:05:46 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ return {
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
-
+ vim.opt.wrap = true,
   -- Set colorscheme to use
   colorscheme = "astrodark",
   -- colorscheme = "octagon",
